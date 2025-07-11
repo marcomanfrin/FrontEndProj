c501a86b93d01aad09bd7765822d7bfc9aab50d5
marco <marco.manfrin04@gmail.com>
2025-07-11 22:38:55 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://bivacchiappserver.onrender.com/api/";
-export const API_URL = "http://localhost:3001";
+export const API_URL = "https://bivacchiappserver.onrender.com/api/";
+//export const API_URL = "http://localhost:3001";
